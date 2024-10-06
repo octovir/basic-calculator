cd8ac10a9cc749256aae3bcba86b32bab705317e
octovir <octovir@gmail.com>
2024-10-06 21:37:54 +0700

    done

---
+++ Calculator/index.js
@@ -0,0 +1,20 @@
+//calculator program ;3
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value= "";
+}
+
+function calculator(){
+    try{
+        display.value= eval(display.value);
+    }
+    catch(error){
+        display.value= "error"
+    }
+}
